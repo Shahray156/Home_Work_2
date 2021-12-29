c5230e6a312539940294f6556d35c3c35dce69b9
Shahray156 <shahray1561@gmail.com>
2021-12-29 23:17:39 +0300

    Второй коммит

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,30 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // задание №1
+        int clientOS = 1;
+        if (clientOS >= 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+            if (clientOS < 1) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+        }
+        // задание №2
+        int clientDevice = 1;
+        int clientDeviceYear = 2015;
+        if ((clientDeviceYear >= 2015) && (clientDevice >= 1)) {
+            System.out.println("Приложение работает корректно");
+        } else {
+            if ((clientDeviceYear <= 2015) && (clientDevice >= 1)) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+            if ((clientDeviceYear >= 2015) && (clientDevice <= 1)) {
+                System.out.println("Приложение работает корректно");
+            } else {
+                if ((clientDeviceYear <= 2015) && (clientDevice <= 1)) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                }
+            }
+        }
     }
 }
